dedef3e66f6fd1f5f0ffde2d9631929e32688d07
TysonFahey <103978328+TysonFahey@users.noreply.github.com>
2024-11-20 14:37:31 -0600

    first commit

---
+++ body-mass-lab-10/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import BodyMassIndex from './components/BodyMassIndex.vue'
+
+import {ref} from 'vue'
+
+const calculateBMI = (height, weight) => {
+  bmi.value = weight / (height * height) 
+} // this function calculates the bmi using data we emited in BodyMassIndex.vue
+
+const bmi = ref(0) // Sets the initial bmi to 0 when user loads the page
+
+</script>
+
+<template>
+ 
+<h1>Body Mass Index Calculator</h1>
+
+<BodyMassIndex v-on:user-entered-data="calculateBMI"></BodyMassIndex> <!-- When the user selects calculate, call the calculateBMI function-->
+
+<div>Your BMI is {{ bmi }}</div> <!-- display the users bmi -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ body-mass-lab-10/src/components/BodyMassIndex.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const userHeight = ref(); // set userHeight to nothing so the user can enter theirs easily ( if you wanted a default for the initial page load before user enters anything you put the number inside of the () ex. ref(1); 1 will be displayed in the input field)
+const userWeight = ref(); // set userWeight to nothing so the user can enter theirs easily
+
+const emit = defineEmits(["user-entered-data"]); // Set up user-entered-data so we can use the emit to send it outside of this component
+
+const userHasSubmittedData = () => {
+  emit("user-entered-data", userHeight.value, userWeight.value);
+}; // this function emits user-entered-data and sends userHeight.value, userWeight.value to be able to be recieved by App.vue
+</script>
+
+<template>
+  <h2>Enter your height and weight</h2>
+
+  <label>Height in meters</label>
+  <input v-model="userHeight" /> <!-- connect the userHeight input using v-model -->
+
+  <br />
+
+  <label>Weight in kilograms</label><input v-model="userWeight" />
+<!-- connect the userWeight input using v-model -->
+  <br />
+
+  <button v-on:click="userHasSubmittedData">Calculate!</button>
+  <!-- set up the button so that it uses v-on:click. when the user clicks the button, userHasSubmittedData function is called.-->
+</template>
+
+<style scoped>
+button {
+  background-color: lightblue;
+}
+</style>
